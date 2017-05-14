00101e0ee60186cdaabe670609956e9395fdb9bb
clofjs <clofsj@ya.ru>
2017-05-14 20:12:19 +0400

    OK!

--- helpfunc_1.js
+++ helpfunc_1.js
@@ -5,33 +5,20 @@ var result=
 
 select: function(req,qr) 
 {
+
      	var query_length = Object.keys(req.query).length;     	
-     			if(query_length > 0 || req.params.id)
+     			if(query_length > 0)
             	{
 	                qr += ' where '
-					if (req.params.id)
-					{
-						qr += 'id = ' + req.params.id + ' '; 
-					}else 
 	                for (var propName in req.query)
 	                {
 	                	if (req.query.hasOwnProperty(propName)) 
-	                    {   
-                            if (propName == 'fromdate' || propName == 'todate')
-                            {
-                                var newprop = req.query[propName];
-                                console.log(Object.keys(newprop));
-                                if (propName == 'fromdate')
-                                    var symb = '>';
-                                else 
-                                    var symb = '<';
-                                qr += Object.keys(newprop) + symb +"= '" + newprop[Object.keys(newprop)[0]] + "'";
-                            }else if (isNaN(req.query[propName])==true){
-	                    		qr += propName + " = '" + req.query[propName]+"'";
-                                console.log(typeof req.query[propName])	;
+	                    {
+	                    	if (isNaN(req.query[propName])==true) 
+	                    	{
+	                    		qr += propName + " = '" + req.query[propName]+"'";	
 	                    	}else{
-	                    		qr +=propName + ' = ' + req.query[propName];
-                                console.log(typeof req.query[propName]) ;	
+	                    		qr +=propName + ' = ' + req.query[propName];	
 	                    	}
 	                     	query_length -= 1;
 	                        if (query_length > 0) 
@@ -39,14 +26,17 @@ select: function(req,qr)
 	                            qr += ' and '
 	                        }
 	                    }	
-	                
-	                    
-	                }
-            }
-            console.log(qr);            
-         	return qr;
-},
+	             	}
 
+	            }else
+	               	if (req.params.id) 
+	               	{
+	               		qr+= 'where id = '+req.params.id+' ';	
+	               	}				
+	           	
+     			
+            	return qr;
+},
 findone: function(req) 
 {   
 	var i=0;
@@ -58,36 +48,38 @@ findone: function(req)
 return i;
 	
 },
+
 upd: function(req,qr,queryid) 
 {	    
 	 
-	if (req.params.id||this.findone(req.body)>0)
+	if (req.params.id&&this.findone(req.body)>0)
 	{
 		for (var propName in req.body) 
        {
 	                    	
            if (isNaN(req.body[propName])==true)
 	        {
-	        	qr += propName + " = '" + req.body[propName]+"',";	
+	        	qr += propName + " = '" + req.body[propName]+"', ";	
 	       	}else{
-	       		qr += propName + ' = ' + req.body[propName]+",";	
+	       		qr += propName + ' = ' + req.body[propName]+" , ";	
 	       	} 
 
 		}
-        qr=qr.substring(0,qr.length-1);
-        if (req.params.id) {
-   		qr+= " where "+queryid+" = "+req.params.id;  
-        } 
+		qr=qr.substring(0,qr.length-2);
 
+   		qr+= " where "+queryid+" = "+req.params.id;   
+
+	}else
+	{
+		qr="";
 	}
-
-  console.log(qr);
-            return qr;
+      return qr;
 },
 
 
 fullinfo: function(table1,table2,field1,req,res,pool)
 {       
+    console.log("start0");
     if (req.params.id)
     {
         pool.connect(function(err, client)
@@ -101,6 +93,7 @@ fullinfo: function(table1,table2,field1,req,res,pool)
             {
                 if(!err)
                {        
+                        console.log("start");
                         client.query('SELECT * FROM ' + table2 + ' where ' + field1 + '  = $1',[req.params.id], function(err, result)
                         {
                             if(!err)
@@ -108,6 +101,7 @@ fullinfo: function(table1,table2,field1,req,res,pool)
                                 var obj1 = result1.rows[0];
                                 var obj2 ={details:result.rows};
                                 Object.assign(obj1, obj2);  
+                                console.log("start1");     
                                 res.json(obj1);     
                             }                               
                             else
